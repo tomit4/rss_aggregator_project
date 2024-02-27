491d2678b6905c2338d72b77db93fd596d496214
tomit4 <mosssap@gmail.com>
2024-02-26 17:21:00 -0800

    :pencil2: Fixed formatting sql files

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,9 +1,11 @@
 -- +goose Up
 
-CREATE TABLE users (id UUID PRIMARY KEY,
-                                    created_at TIMESTAMP NOT NULL,
-                                                         updated_at TIMESTAMP NOT NULL,
-                                                                              name TEXT NOT NULL);
+CREATE TABLE users(
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    name TEXT NOT NULL
+);
 
 -- +goose Down
 

30e89b26eed4e1eca1d4057837048131f37dd14f
tomit4 <mosssap@gmail.com>
2024-02-26 18:04:59 -0800

    :construction: Continuing, now adding feeds

--- handler_user.go
+++ handler_user.go
@@ -3,12 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/google/uuid"
+	"github.com/tomit4/rssagg/internal/database"
 	"net/http"
 	"time"
-
-	"github.com/google/uuid"
-	"github.com/tomit4/rssagg/internal/auth"
-	"github.com/tomit4/rssagg/internal/database"
 )
 
 // Adds a method to the apiConfig struct to handle user creation
@@ -38,17 +36,6 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 	respondWithJSON(w, 201, databaseUserToUser(user))
 }
 
-func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
-	apiKey, err := auth.GetAPIKey(r.Header)
-	if err != nil {
-		respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
-		return
-	}
-
-	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
-	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Couldn't get user: %v", err))
-		return
-	}
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request, user database.User) {
 	respondWithJSON(w, 200, databaseUserToUser(user))
 }
